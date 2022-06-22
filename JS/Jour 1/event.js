0947d62bdb5242c4dc60303cc1d163ed992a4ade
Clément <clement.delord@epitech.eu>
2022-06-22 14:10:49 +0200

    ex filtre

---
+++ JS/Jour 1/event.js
@@ -0,0 +1,9 @@
+// document.getElementById("btn").addEventListener('click', (event) => {
+//     console.log(event);
+// });
+
+function log () {
+    if (confirm('Button was pressed !')) {
+        console.log('Yes !');
+    }
+}
