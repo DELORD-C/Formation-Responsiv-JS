645b5e6e3d21b9fa760c4bfbbebd25310130fe76
Clément <clement.delord@epitech.eu>
2022-06-23 10:39:14 +0200

    ex form corrigé

--- JS/Jour 2/Stop/script.js
+++ JS/Jour 2/Stop/script.js
@@ -2,7 +2,29 @@ let form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    //effectuer nos vérifications sur le formulaire
-    //si nos vérification son justes alors on envoi le formulaire
-    form.submit();
+    let name = form.firstElementChild,
+        mail = name.nextElementSibling,
+        regexname = /.{4}.*/,
+        regexmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        errors = 0;
+
+    if (!regexname.test(name.value)) {
+        errors++;
+        name.style.border = 'solid 1px red';
+    }
+    else {
+        name.style.removeProperty('border');
+    }
+
+    if (!regexmail.test(mail.value)) {
+        errors++;
+        mail.style.border = 'solid 1px red';
+    }
+    else {
+        mail.style.removeProperty('border');
+    }
+        
+    if (errors === 0) {
+        form.submit();
+    }
 });
